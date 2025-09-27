1487609f6c8c6986f86ef6cd46ebb05bc1891298
luisitcho <luishenrisc1@gmail.com>
2025-09-27 03:09:58 -0300

    Projeto Spotify - Album

---
+++ src/main/java/tech/buildrun/spotify/controller/AlbumController.java
@@ -0,0 +1,16 @@
+package tech.buildrun.spotify.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/spotify/api")
+public class AlbumController {
+
+    @GetMapping("/albums")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok().build();
+    }
+}
