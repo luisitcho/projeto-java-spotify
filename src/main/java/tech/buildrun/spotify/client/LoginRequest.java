bdadc90f904184d31913a5ffdd5ecbe4568d570f
luisitcho <luishenrisc1@gmail.com>
2025-09-27 03:41:15 -0300

    Projeto Spotify - LoginRequest

---
+++ src/main/java/tech/buildrun/spotify/client/LoginRequest.java
@@ -0,0 +1,41 @@
+package tech.buildrun.spotify.client;
+
+public class LoginRequest {
+
+    private String grantType;
+    private String clientId;
+    private String clientSecret;
+
+    public LoginRequest() {
+    }
+
+    public LoginRequest(String grantType, String clientId, String clientSecret) {
+        this.grantType = grantType;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+    }
+
+    public String getGrantType() {
+        return grantType;
+    }
+
+    public void setGrantType(String grantType) {
+        this.grantType = grantType;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+}
